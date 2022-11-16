14ce67d0a3ee13c894923edeab10daa70ca8b20e
Cesar Eduardo Muñoz Chavez <cesareduardomucha@hotmail.com>
2022-11-15 22:34:14 -0600

    Generation data 1.0

--- Final_Code_0_3_Generation_Data.py
+++ Final_Code_0_3_Generation_Data.py
@@ -8,48 +8,136 @@ from Final_Code_0_0_Libraries import randint
 from Final_Code_0_1_Utilities import Utilities
 
 class Generator(Utilities):
+    """
+    Utilities inheritance
+
+    A class used to generate folders and data from students
+
+    Methods:
+        create_folders(): description
+
+        creating_data_students(): description
+
+    """
 
     def __init__(self, **kwargs) -> None:
-
+        """
+        Keyword Args:
+            folder (str): description 
+            FN (list): description
+            iter (int): description
+            SD (bool): description
+        """
         # * Instance attributes (Private)
         self.__Folder_path = kwargs.get('folder', None);
         self.__Folders_name = kwargs.get('FN', None);
         self.__Iteration = kwargs.get('iter', None);
         self.__Save_dataframe = kwargs.get('SD', None);
 
-    # ? Create folders
+    def __repr__(self):
+        return f'[{self.__Folder_path}, {self.__Folders_name}, {self.__Iteration}, {self.__Save_dataframe}]';
+
+    def __str__(self):
+        return  f'A class used to generate folders and data from students';
+    
+    # * Deleting (Calling destructor)
+    def __del__(self):
+        print('Destructor called, Generator class destroyed.');
+
+    def data_dic(self):
+
+        return {'Folder path': str(self.__Folder_path),
+                'Folders names': str(self.__Folders_name),
+                'Iteration': str(self.__Iteration),
+                'Dataframe': str(self.__Save_dataframe),
+                };
+
+    # * __Folder_path attribute
+    @property
+    def __Folder_path_property(self):
+        return self.__Folder_path;
+
+    @__Folder_path_property.setter
+    def __Folder_path_property(self, New_value):
+        self.__Folder_path = New_value;
+    
+    @__Folder_path_property.deleter
+    def __Folder_path_property(self):
+        print("Deleting folder path...");
+        del self.__Folder_path;
+    
+    # * __Folders_name attribute
+    @property
+    def __Folders_name_property(self):
+        return self.__Folders_name;
+
+    @__Folders_name_property.setter
+    def __Folders_name_property(self, New_value):
+        self.__Folders_name = New_value;
+    
+    @__Folders_name_property.deleter
+    def __Folders_name_property(self):
+        print("Deleting folder name or folder names..");
+        del self.__Folders_name;
+
+    # * __Iteration attribute
+    @property
+    def __Iteration_property(self):
+        return self.__Iteration;
+
+    @__Iteration_property.setter
+    def __Iteration_property(self, New_value):
+        self.__Iteration = New_value;
+    
+    @__Iteration_property.deleter
+    def __Iteration_property(self):
+        print("Deleting iteration values..");
+        del self.__Iteration;
+
+    # * __Save_dataframe
+    @property
+    def __Save_dataframe_property(self):
+        return self.__Save_dataframe;
+
+    @__Save_dataframe_property.setter
+    def __Save_dataframe_property(self, New_value):
+        self.__Save_dataframe = New_value;
+    
+    @__Save_dataframe_property.deleter
+    def __Save_dataframe_property(self):
+        print("Deleting save dataframe value..");
+        del self.__Save_dataframe;
+
+    # ? Method to create folders
     @Utilities.timer_func
     def create_folders(self) -> pd.DataFrame: 
         """
-        _summary_
+        Method used to create folders 
 
-        _extended_summary_
-
-        Args:
-            Folder_path (str): _description_
-            Folder_name (list[str]): _description_
-            CSV_name (str): _description_
         """
 
-        # *
+        # * Create the list to save names and absolute dir
         Path_names = [];
         Path_absolute_dir = [];
 
-        # *
+        # * if multiple folders name in the list are included
         if(len(self.__Folders_name) >= 2):
             
-            # *
+            # * Enumerate each one
             for i, Path_name in enumerate(self.__Folders_name):
 
-                # *
+                # * Combine the name with the dir
                 Folder_path_new = r'{}\{}'.format(self.__Folder_path, self.__Folders_name[i]);
                 print(Folder_path_new);
 
+                # * Append names and absolute dir
                 Path_names.append(Path_name);
                 Path_absolute_dir.append(Folder_path_new);
 
+                # * Check if the dir is already created
                 Exist_dir = os.path.isdir(Folder_path_new) ;
 
+            # * if not create the new folder
             if Exist_dir == False:
                 os.mkdir(Folder_path_new);
             else:
@@ -57,21 +145,24 @@ class Generator(Utilities):
 
         else:
 
-            # *
+            # * Combine the name with the dir
             Folder_path_new = r'{}\{}'.format(self.__Folder_path, self.__Folders_name);
             print(Folder_path_new);
-
+            
+            # * Append names and absolute dir
             Path_names.append(self.__Folders_name);
             Path_absolute_dir.append(Folder_path_new);
 
+            # * Check if the dir is already created
             Exist_dir = os.path.isdir(Folder_path_new) ;
 
+            # * if not create the new folder
             if Exist_dir == False:
                 os.mkdir(Folder_path_new);
             else:
                 print('Path: {} exists, use another name for it'.format(self.__Folders_name));
 
-        # *
+        # * Save the name inside a new dataframe
         if(self.__Save_dataframe == True):
             Dataframe = pd.DataFrame({'Names':Path_names, 'Path names':Path_absolute_dir});
             Dataframe_name = 'Dataframe_path_names.csv'.format();
@@ -83,10 +174,14 @@ class Generator(Utilities):
 
         return Dataframe
 
-    # ? Create folders
+    # ? Method to create folders
     @Utilities.timer_func
     def creating_data_students(self) -> pd.DataFrame: 
-        
+        """
+        Method used to create random students data 
+
+        """
+
         # * Tuples for random generation.
         Random_Name = ('Tom', 'Nick', 'Chris', 'Jack', 'Thompson');
         Random_Classroom = ('A', 'B', 'C', 'D', 'E');
@@ -94,12 +189,12 @@ class Generator(Utilities):
         # * Column names to create the DataFrame
         Columns_names = ['Name', 'Age', 'Classroom', 'Height', 'Math', 'Chemistry', 'Physics', 'Literature'];
 
-        # *
+        # * Create a new dataframe using the headers
         Dataframe = pd.DataFrame(Columns_names)
 
         for i in range(self.__Iteration):
 
-            # *
+            # * Create a new row randomly
             New_row = { 'Name':random.choice(Random_Name),
                         'Age':randint(16, 26),
                         'Classroom':random.choice(Random_Classroom),
@@ -111,10 +206,10 @@ class Generator(Utilities):
 
             Dataframe = Dataframe.append(New_row, ignore_index = True);
 
-            # *
+            # * Show each iteration
             print('Iteration complete: {}'.format(i));
 
-        # *
+        # * Save the name inside a new dataframe
         if(self.__Save_dataframe == True):
             Dataframe_name = 'Dataframe_students_data.csv'.format();
             Dataframe_folder = os.path.join(self.__Folder_path, Dataframe_name);
